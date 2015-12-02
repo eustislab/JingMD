c3874c30f5bcaae96651645cbf5b9b29daf98cfe
jingsk <ktreraya@bowdoin.edu>
2015-12-02 14:42:49 -0500

    Update achemso.tex
    write plotEnergyMD section

--- pythonScripts/plotEnergyMD6.py
+++ pythonScripts/plotEnergyMD6.py
@@ -1,7 +1,7 @@
 ###########################################################
-###This version is created to deal with large file      ###
-###The solution will be to use for line in f1 and avoid ###
-###using readlines()                                    ###
+### Use this to plot energy vs time to see if MD has    ###
+### run its course. Generates: csv of PE and            ###
+### temperature vs time, pdf of the plot                ###
 ###########################################################
 
 import matplotlib
@@ -12,7 +12,7 @@ import sys
 import string
 
 #call as plotEnergyMD3.py inputfile
-
+#for asking what the input in terminal should be
 try:
 	if str(sys.argv[1])=='?':
 		print '\nCall function as: plotEnergyMD.py input.log   \n' 
@@ -22,13 +22,9 @@ try:
 except IndexError:
     print '\n!!!Input command Error. Call function as: plotEnergyMD.py input.log \n' 
     sys.exit()
-
 output=str(input) + '_energies.csv'
-###############################
-#clear (input).energies.csv
-f = open(output, 'w');
-f.write('')
-#constant
+
+#initiate variables
 lineBwTimeAndEnergy=0;
 lineBwTimeAndTemp=0;
 lineCountFromTime=0;
@@ -41,11 +37,8 @@ grandString=''
 
 ###############################
 #functions
-def printTime (thisLine):
-    lineComponents=thisLine.split();
-    timeString=str(lineComponents[3]);
-    print "Analyzing t = "+timeString+" fsec\n"
-    
+
+#check if time is in line
 def shouldCollectTime(line):
     #only check if collection is in progress
     if ' *** AT T=' in line:
@@ -53,29 +46,36 @@ def shouldCollectTime(line):
     else:
         return False;
 
+# for printing time so one can keep track of the progress
+def printTime (thisLine):
+    lineComponents=thisLine.split();
+    timeString=str(lineComponents[3]);
+    print "Analyzing t = "+timeString+" fsec\n"
+
+#Are we currently looking potential energy?
 def shouldCollectPE(line,reference,currentLine):
-    #check if line bw time and energy is known 
+    #check if line bw time and energy is known - this is written as reference
     if reference>0:
         if currentLine==reference:
             return True
         else:
             return False
-    #if not then it needs to be found using string
+    #if reference is not known, then it needs to be found by finding string POT EN...
     elif reference==0:
         if '     POT  ENERGY' in line:
             reference=int(currentLine)
             return True;
         else:
             return False;
-
+#Are we currently looking Temp?
 def shouldCollectTemp(line,reference,currentLine):
-    #check if line bw time and energy is known 
+    #check if line bw time and energy is known - reference
     if reference>0:
         if currentLine==reference:
             return True
         else:
             return False
-    #if not then it needs to be found using string
+    #if not then it needs to be found by searching for the string TEMPER...
     elif reference==0:
         if '     TEMPER' in line:
             reference=int(currentLine)
@@ -83,6 +83,7 @@ def shouldCollectTemp(line,reference,currentLine):
         else:
             return False;
 
+#once everything is found, we should write down before moving on to the next snapshot
 def shouldWrite():
     #only check if collection is in progress
     if foundTime:
@@ -97,13 +98,16 @@ def shouldWrite():
         return False;
 
 ###############################
+#open csv and prepare for writing
 f = open(output, 'w');
 f.write('')
+
 #open input
-print 'finding patterns...'
 f1=open(input)
+print 'finding patterns...'
+#avoid using readlines() so there'll be no problem with large files
 for line in f1:
-    #this keyword is usually before coordinate
+    #time keyword is before PE, PE is before Temp so the search should be in this order in order to be most efficient
     #find out if collection is needed            
     if not foundTime:
         if shouldCollectTime(line):
@@ -132,13 +136,18 @@ for line in f1:
                 lineComponents=line.split();
                 grandString=grandString+', '+str(lineComponents[2])+'\n';
                 foundTemp=True
+        #problem with this is - the first snapshot's temperature is not given in the log file
+        #set Temp to 0 to indicate the beginning
         if firstTime:
             grandString=grandString+',0\n';
+            #once append, turn off the boolean
             firstTime=False
             foundTemp=True
+    #write after all data is collected for one snapshot
     if shouldWrite():
         with open(output, 'a') as f:
             f.write(grandString)
+        #reset the variables
         lineCountFromTime=0;
         collectionStarted=False
         foundTime=False
@@ -146,9 +155,12 @@ for line in f1:
         foundTemp=False
         grandString=''
     lineCountFromTime+=1;
+#finish writing csv
+f.close()
 
-
+#plot
 #pull out CSV
+#use csv.reader bc csv has ',' and this automate the formatting
 f = csv.reader(open(output))
 #convert column to array using zip (a built in function)
 Time, Energy, Temp = zip(*f)
@@ -174,26 +186,6 @@ ax1.set_ylabel('Potential Energy (KCal/mol)', color='g')
 ax1.ticklabel_format(axis='y', style='sci', scilimits=(-2,2), useOffset=False)
 ax2.set_ylabel('System Temperature (K)', color='b')
 plt.title(r'       Potential Energy vs Time for the MD File: ' + str(input))
+#Saving to pdf gives better resolution - picture is saved to vector
+#there is a room for improvement especially these energy plots which look very mediocre 
 plt.savefig(str(input) + '_EnergyPlot.pdf', format='pdf')
-
-#find Equilibrium - linear regression
-print 'Finding Equilibrium'
-try:
-    minNumberOfStep=1000
-    #this is from aniline32.log - 15000 to 25000
-    maxSlope=0.00022
-    size=len(Time)
-    x=Time[0:minNumberOfStep-1]
-    y=Energy[0:minNumberOfStep-1]
-    for i in range(0,size-1):
-        print 'Analyzing at t= '+Time[i]
-        m,b = np.polyfit(x, y, 1)
-        if m<=0.00022:
-            break
-            print 'Found equilibrium starting from '+Time[i]+' to '+Time[i+minNumberOfStep]
-        #else remove the head and add next tail
-        else:
-            del x[0]
-            x.insert(-1,Energy(minNumberOfStep+i))
-except IndexError:
-        print 'There are not enough data to determine the equilibrium'
