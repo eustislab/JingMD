31fc6bb6939b414814c660c35b78d43ee0f3c4ea
jingsk <jing_sk129@hotmail.com>
2015-12-02 19:03:20 -0500

    update achemso.tex and py
    work as of Dec 2. Need to work more on intro and methods but appendix
    is done

--- pythonScripts/findEquilibrium.py
+++ pythonScripts/findEquilibrium.py
@@ -1,12 +1,16 @@
+###########################################################
+### This is used to determined the equilibrium using    ###
+### linear regression and an upper limit for the slope	###
+###########################################################
+
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import csv
 import sys
-import string
 import numpy as np
-#if this is called from plotEnergyMD then there is no need to ask for output name and redefine the variables again
 
+#for asking what the input in terminal should be
 try:
     if str(sys.argv[1])=='?':
         print '\nCall function as: findEquilibrium.py input.log   \n' 
@@ -17,6 +21,7 @@ except IndexError:
     print '\n!!!Input command Error. Call function as: findEquilibrium.py input.log \n' 
     sys.exit()
 output=str(input) + '_energies.csv'
+#This portion is the same as in plotEnergyMD6
 #pull out CSV
 f = csv.reader(open(output))
 #convert column to array using zip (a built in function)
@@ -26,39 +31,57 @@ Time = map(float, Time)
 Energy = map(float, Energy) 
 Temp = map(float, Temp) 
 
-#find Equilibrium - linear regression
+#find Equilibrium using linear regression
+#this number control the range of time to be used in energy fluctuation calculation
 minNumberOfStep=1000
+#This is the limit above which the script will report no equilibrium is found 
 #this is from aniline32.log - 15000 to 25000
 maxSlope=1e-4
 #for plotting
 slope=[]
-print 'Finding equilibrium using minimum number of steps = '+str(minNumberOfStep) +' and maximum slope = '+str(maxSlope)
+print 'Finding equilibrium using minimum number of steps = '+str(minNumberOfStep) +' and top limit of acceptable slope = '+str(maxSlope)
 try:
+	#for looping
     size=len(Time)
     x=Time[0:minNumberOfStep]
     y=Energy[0:minNumberOfStep]
     #for using in loop
+    #set thio a high value - it can be any number bc we will replace it with the lowest slope value found in loop
     lowestSlopeValue=1e5
+    #Same - this will be replaced
     indexOfLowestSlope=-1
     for i in range(0,size-1-minNumberOfStep):
         print 'Finding equilibrium from t= '+str(Time[i])+' to '+str(Time[i+minNumberOfStep-1])
+        #poly fit is basically a linear fit - m=slope, b=y_intersect
         m,b = np.polyfit(x, y, 1)
+        #for plotting - append to array of existing slope values
         slope.append(m)
         print 'slope = ' +str(m)
+        #take absolute value and see which interval does not fluctuate the least
         if abs(m)<=abs(lowestSlopeValue):
             lowestSlopeValue=float(m)
             indexOfLowestSlope=int(i)
+        #this is similar to queue structure - room for improvement is to make x and y arrays into actual queues
         #else remove the head and add next tail
         del x[0]
         x.append(Time[minNumberOfStep+i])
         del y[0]
         y.append(Energy[minNumberOfStep+i])
+#if x or y does not have enough element (minNumberOfStep) then report error
+#room for improvement - move this up top instead of having a long try
 except IndexError:
         print 'There is not enough data to determine the equilibrium'
+#if lowestSlopeValue pass the top limit then report
 if abs(lowestSlopeValue)<=abs(maxSlope):
-        print 'Found best equilibrium starting from '+str(Time[indexOfLowestSlope])+' to '+str(Time[indexOfLowestSlope+minNumberOfStep-1])+' with slope = '+str(lowestSlopeValue)
-
+    print 'Found best equilibrium starting from '+str(Time[indexOfLowestSlope])+' to '+str(Time[indexOfLowestSlope+minNumberOfStep-1])+' with slope = '+str(lowestSlopeValue)
+#if not then say so
+else:
+	print 'Equilibrium is not yet reach.' '
+	print 'The current limit is at '+str(maxSlope) +' kcal/mol/fs and the lowest value of slope = '+str(lowestSlopeValue)
+	
+#plot slope vs time
 x=slope
+#align time with slope
 y=Time[0:size-1-minNumberOfStep]
 plt.plot(y,x)
 plt.xlabel('Starting Time (fs)')
--- pythonScripts/postMDDataPull2.py
+++ pythonScripts/postMDDataPull2.py
@@ -1,8 +1,13 @@
+###########################################################
+### Use this pull out excited state energies and dipole ###
+### moments from all the files in specified folder      ###
+### Output is time, S1, S2... or time, X1, Y1, Z1, X2...###
+###########################################################
+
 import os as os
 import numpy as numpy
 import matplotlib.pyplot as plt
 import sys
-
 
 #if not sure use ? to ask
 try:
@@ -16,15 +21,19 @@ except IndexError:
 #Call as fincut.py input.log numberOfSoluteAtoms numberofSolventAtoms numberOfSolventMolecules startTime stopTime timePerFrame
 try:
     input=str(sys.argv[1])
-numberOfExcitedStates=int(sys.argv[2])
+	numberOfExcitedStates=int(sys.argv[2])
 except IndexError:
     print '\n!!!Input command Error. Call function as: postMDDataPull2.py inputDirectory NumberOfExcitedStateEnergies\n' 
     sys.exit()
+# to prevent / at the end of the input file if used terminal autofill
+if input.endswith('/'):
+    input = input[:-1]
 
-
+#make input path - folder containing the log files or out files
 path = os.getcwd()
 inputPath=path+'/'+input
 
+#arrays for storing and manipulating the data extracted
 outputList=[]
 fileList=os.listdir(inputPath)
 listAllEAndf=[]
@@ -33,43 +42,34 @@ oscillatorStrengths=[]
 timeList=[]
 energyFinal=numpy.array(['Energy(eV)'])
 oscillatorStrengthFinal=numpy.array(['Oscillator Strength'])
-def mode(list):
-    d = {}
-    for elm in list:
-        try:
-            d[elm] += 1
-        except(KeyError):
-            d[elm] = 1    
-    keys = d.keys()
-    max = d[keys[0]]
-    for key in keys[1:]:
-        if d[key] > max:
-            max = d[key]
-    max_k = []      
-    for key in keys:
-        if d[key] == max:
-            max_k.append(key),
-    return max_k,max
+    
+#get all the file names
 for fileName in fileList:
     if (".out" in fileName or ".log" in fileName):
         outputList.append(fileName)
 
+#open 2 output files
 f1=open(input+'MD_data.csv','w')
 f2=open(input+'MD_dipole.csv','w')
+#now read each file and collect data
 for fileName in outputList:
 	eachFile=inputPath+'/'+fileName
 	f=open(eachFile,'r')
 	lines=f.readlines()
 	startingLine=0
 	fileIsComplete=False
+	#find if the file is complete
+	# use enumerate and reverse
 	for i, line in reversed(list(enumerate(lines))):
 		if ' STATE #   1  ENERGY =    ' in line:
 			startingLine=i
 			fileIsComplete=True
 			break
+	# now start collecting data
 	if fileIsComplete:
 		print 'Harvesting data from file named: '+fileName
 		counter=0
+		#loop
 		for n, line in enumerate(lines):
 			#time
 			if line.startswith('     RUN TITLE'):
@@ -77,22 +77,24 @@ for fileName in outputList:
 				time=str(nextLine.split()[3])
 				f1.write(time)
 				f2.write(time)
+			#energy
 			if line.startswith(' STATE # '):
 				E=line.split()[5]
 				nextLine=lines[n+1]
 				f=nextLine.split()[3]
 				f1.write(','+E+','+f)
 				counter+=1
+			#stop collecting
 			if (counter==numberOfExcitedStates):
 				f1.write('\n')
 				counter=0
+			#dipole
 			if line.startswith('                          SUMMARY OF TDDFT RESULTS'):
+				#5 lines are from 'SUMMARY...' to ' 1  A ...' which starts to contain dipole moments
 				for linesAhead in range(5,5+numberOfExcitedStates):
 					lineContainingDipoles=lines[n+linesAhead]
 					lineSplit=lineContainingDipoles.split()
+					#locations of dipole in line 
 					[X,Y,Z]=[lineSplit[4], lineSplit[5], lineSplit[6]]
 					f2.write(','+X+','+Y+','+Z)
-				f2.write('\n')
-    			
-    			
-    			
+				f2.write('\n')	
